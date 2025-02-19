e0ea2e257b904c2763f2ece8aace4f31366b1d12
DarioCaramiello <dario.caramiello001@studenti.uniparthenope.it>
2025-02-19 11:02:40 +0100

    profilo.js removed 2

--- static/js/profilo.js
+++
@@ -1,8 +0,0 @@
-function show() {
-    var x = document.getElementById("password_profile");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  }
